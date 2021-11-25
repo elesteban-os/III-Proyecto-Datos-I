2296b0dff2299eb8457fdb6e680e1ec8d9ea651d
elesteban-os <esteban25agosto@gmail.com>
2021-11-24 19:01:39 -0600

    Interfaz y dibujos

--- PathFinder/src/UI/Draw.java
+++ PathFinder/src/UI/Draw.java
@@ -1,5 +1,15 @@
 package UI;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
 
-public class Draw {
-    
+public class Draw extends JPanel {
+
+    public void drawCircles(Graphics g, int x, int y, Color color){
+        g.setColor(color);
+        g.fillOval(x, y, 10, 10);   
+         
+    }
+
+    //public void drawLines(Graphics g, int x, int y,)
 }
--- PathFinder/src/UI/UserInterface.java
+++ PathFinder/src/UI/UserInterface.java
@@ -1,50 +1,63 @@
 package UI;
 
-import java.awt.Font;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class UserInterface extends JFrame{
+    
     private JFrame window = new JFrame("Path Finder");
     private JLabel map = new JLabel();
     private JLabel title = new JLabel();
     private JLabel origen = new JLabel();
     private JLabel destino = new JLabel();
-    
+    private Draw drawing = new Draw();
+    ImageIcon mapIcon = new ImageIcon(getClass().getResource("/Images/map.png"));
+    private JPanel paper = new JPanel(){
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.drawImage(mapIcon.getImage(), 0, 0, 600, 600, null);
+            drawNodes(g, Color.BLACK);
+        }
+    };
+
+
     private String[] places = {"San José", "Alajuelita", "Escazú", "Desamparados", "Puriscal", "Tarrazú", "Aserrí", "Goicoechea", 
                                "Santa Ana", "Colorado", "Acosta", "Tibás", "Montes de Oca", "Pérez Zeledón", "Curridabat"};
+    //                       1    2    3    4    5    6    7    8    9    10   11   12   13   14   15
+    private int[] xPlaces = {205, 246, 230, 284,  98, 286, 245, 320, 204, 340, 185, 262, 286, 440, 290};
+    private int[] yPlaces = {149, 175, 159, 187, 251, 327, 247, 127, 150,  91, 247, 126, 142, 439, 152};
+
                             
     private JComboBox<String> city1 = new JComboBox<>(places);
     private JComboBox<String> city2 = new JComboBox<>(places);
 
     private JButton calculate = new JButton("Calcular");
 
-    ImageIcon mapIcon = new ImageIcon(getClass().getResource("/Images/map.png"));
 
-    private JPanel nodes = new JPanel(){
-        public void paintComponent(Graphics g){
-            super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D) g;
-            Ellipse2D ellipse = new Ellipse2D.Double(100, 100, 100, 100);
-            g2.draw(ellipse);
+    public void drawNodes(Graphics g, Color color){
+        int len = this.xPlaces.length;
+        for (int i = 0; i < len; i++){
+            this.drawing.drawCircles(g, this.xPlaces[i], this.yPlaces[i], color); 
+        }
+    }    
+
+    //dhjsjdhgagshjk
+    ActionListener colors = new ActionListener(){
+        public void actionPerformed(ActionEvent event){
+            drawing.drawCircles(paper.getGraphics(), 100, 100, Color.BLUE);
         }
     };
-    
 
     public UserInterface() {
-        // Mapa
-        this.map.setIcon(this.mapIcon);
-        this.map.setBounds(150, 0, 733, 711);
-
-
         // ComboBox
         this.city1.setBounds(10, 120, 150, 30);
         this.city2.setBounds(10, 210, 150, 30);
 
         // Botones
         this.calculate.setBounds(10, 250, 150, 30);
+        //this.calculate.addActionListener(colors);
 
         // Labels
         this.title.setText("PATH FINDER");
@@ -59,7 +72,11 @@ public class UserInterface extends JFrame{
         this.destino.setBounds(10, 180, 100, 40);
         this.destino.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
 
-        // Add
+        // Paper
+        this.paper.setBounds(250, 0, 1000, 720);
+
+        // Add          
+        this.window.add(this.paper);
         this.window.add(this.city1);
         this.window.add(this.city2);
         this.window.add(this.map);
@@ -67,10 +84,10 @@ public class UserInterface extends JFrame{
         this.window.add(this.origen);
         this.window.add(this.destino);
         this.window.add(this.calculate);
-        this.window.add(this.nodes);
 
        // Window
         this.window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+        this.window.setLayout(null);
         this.window.setSize(1000, 720);
         this.window.setResizable(false);
         this.window.setVisible(true);
@@ -80,7 +97,6 @@ public class UserInterface extends JFrame{
 
     public static void main(String[] args){
         UserInterface user = new UserInterface();
-
-        
+        System.out.println(user.map.getGraphics());
     }
 }
