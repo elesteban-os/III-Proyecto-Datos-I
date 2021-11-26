5216315844eca919d00236a0fe6c98d919870cd3
elesteban-os <esteban25agosto@gmail.com>
2021-11-25 20:38:05 -0600

    Clase main

---
+++ PathFinder/src/main.java
@@ -0,0 +1,14 @@
+import UI.UserInterface;
+
+/**
+ * Clase para ejecutar la aplicación.
+ */
+public class main {
+    /**
+     * Función que abre la interfaz de usuario y todas las características que necesita.
+     * @param args
+     */    
+    public static void main(String[] args) {
+        UserInterface user = new UserInterface();
+    }   
+}
